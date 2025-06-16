d606a8e90fce6ea276ef57c7d208169f7b39b6c1
herin26it <phuonglinh2612004@gmail.com>
2025-06-16 10:01:30 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Component1 from './Component/Component1/Component1';
+import Component2 from './Component/Component2/Component2';
+import "./App.css"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Component1 value={""}
+      onChange ={(e)=>{}}
+      placeholder={""} />
+      <Component2/>
     </div>
   );
 }
